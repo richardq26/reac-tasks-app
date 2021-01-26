d76c9b6a6721dad96d10f90847ec3b41d1727b85
Aicos <richard.sw268@gmail.com>
2021-01-25 22:37:03 -0500

    handles

--- src/App.js
+++ src/App.js
@@ -11,11 +11,36 @@ class App extends Component {
     this.state = {
       todos
     }
+    this.handleAddTodo = this.handleAddTodo.bind(this);
+   
+  }
+  
+  // Manejador de agregar tareas
+  handleAddTodo(todo){
+    // Actualizamos el estado de las tareas
+    this.setState({
+      // Agrego nueva tarea al arreglo de tarea, agrego todo a todos
+      // Los spread(...) sirven para que solo devuelva argumentos del array,
+      // en este caso como usamos [] tenemos que usar el spread para que adentro solo tenga argumentos
+      todos:[...this.state.todos, todo]
+    })
+  }
+  // Eliminar tarea
+  removeTodo(index){
+    // Le agregamos el window para que react sepa que es un evento propio del navegador
+    if(window.confirm('Seguro de querer eliminar?')){
+      // Filter recorre todo y si el dato no cumple la condición lo elimina
+      this.setState({todos: this.state.todos.filter((e,i)=>{
+        // Si el indice no es igual al que le paso, se queda, sino se elimina
+        return i !== index;
+      })
+    });
+    } 
   }
   render(){
     const todosconst= this.state.todos.map((todo,i)=>{
       return(
-        <div className="col-md-4 mt-4">
+        <div className="col-md-4 mt-4" key={i}>
           <div className="card">
           <div className="card-header">
             <h3>{todo.title}</h3>
@@ -24,6 +49,10 @@ class App extends Component {
           <div className="card-body">
             <p>{todo.description}</p>
             <p><mark>{todo.responsible}</mark></p>
+          </div>
+          <div className="card-footer">
+            {/* Este se bind se usa aquí para que no se ejecute en el constructor*/ }
+            <button className="btn btn-danger" onClick={this.removeTodo.bind(this, i)}>Eliminar</button>
           </div>
          
         </div>
@@ -34,14 +63,13 @@ class App extends Component {
     return (
     <div className="App">
       <Navigation titulo="App de tareas con React" tasks={this.state.todos.length}/>
-      
-      
       <div className="container">
-        
         <div className="row mt-4">
           <div className="col-md-4 text-center">
             <img src={logo} className="App-logo" alt="logo" />
-            <TodoForm />
+             {/* onAddTodo es un evento que acabos de crear 
+             Le estamos dando una nueva propiedad llamada onAddTodo*/}
+            <TodoForm onAddTodo={this.handleAddTodo} />
           </div>
           <div className="col-md-8">
             <div className="row">{todosconst}</div>
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -3,24 +3,49 @@ import React, { Component } from 'react';
 class TodoForm extends Component {
   constructor () {
     super();
+    // tiene que ser state, es una propiedad de react
     this.state = {
       title: '',
       responsible: '',
       description: '',
       priority: 'low'
     };
+
+    // Para decirle que se enlace a este componente
+    // Esto es cuando hagamos algo con el state, por ejm mostrarlo
+    this.handleInput = this.handleInput.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  // Evento para recibir los datos que se introducen en el input
+  handleInput(e){
+    // console.log(e.target.value, e.target.name); el valor, el name del input que se modifica
+    const { value, name } = e.target;
+    // Este evento nos permite alterar los valores de state
+    this.setState({ // Cambiamos el dato del inicio
+      [name]: value
+    });
+    console.log(this.state)
+  }
+
+  handleSubmit(e){
+    // preventDefault evita recargar la página
+    e.preventDefault();
+    // Acá usamos la propiedad onAddTodo de App.js
+    this.props.onAddTodo(this.state);
+    console.log('Sending the data..');
   }
 
   render() {
     return (
       <div className="card">
-        <form className="card-body">
+        <form className="card-body" onSubmit={this.handleSubmit}>
           <div className="form-group">
             <input
               type="text"
               name="title"
               className="form-control"
-              
+              onChange={this.handleInput}
               placeholder="Title"
               />
           </div>
@@ -29,7 +54,7 @@ class TodoForm extends Component {
               type="text"
               name="responsible"
               className="form-control"
-              
+              onChange={this.handleInput}
               placeholder="Responsible"
               />
           </div>
@@ -38,7 +63,7 @@ class TodoForm extends Component {
               type="text"
               name="description"
               className="form-control"
-              
+              onChange={this.handleInput}
               placeholder="Description"
               />
           </div>
@@ -46,7 +71,7 @@ class TodoForm extends Component {
             <select
                 name="priority"
                 className="form-control"
-                
+                onChange={this.handleInput}
               >
               <option>low</option>
               <option>medium</option>
